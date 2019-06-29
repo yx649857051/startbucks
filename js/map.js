92852fba4f5ea13761b0a7708d3f58d09564eee6
wangchuang714 <wangchuang714@163.com>
2019-06-29 09:43:25 +0800

    王闯上传，与大佬代码合并，完善页面左侧导航栏功能

--- js/map.js
+++ js/map.js
@@ -29,7 +29,7 @@ AMap.plugin(["AMap.Geolocation"], function() {
 });
 
 
-//增加放大缩小按钮
+//添加增加放大缩小按钮
 AMap.plugin(['AMap.ToolBar'], function() {
     // 在图面添加工具条控件，工具条控件集成了缩放、平移、定位等功能按钮在内的组合控件
     map.addControl(new AMap.ToolBar({
@@ -38,11 +38,88 @@ AMap.plugin(['AMap.ToolBar'], function() {
     }));
 });
 
+// 地图JS结束-->-->-->地图JS结束-->-->-->地图JS结束-->-->-->地图JS结束-->-->-->
+
+
+
+// 左侧导航js
 $('.map_icon').click(function() {
     // alert('首页');
     location.href = "starbucks_home.html";
+
 });
 
+//左侧栏右边的菜单按钮事件
 $('.map_menu_ic').click(function() {
-    alert('菜单');
+
+    $('.map_side_all_default').hide();
+    $('.map_side_all_menu').show();
+    // $('.map_side_all_menu').show(300, 'easeInQuad', function() {
+
+    // });
+});
+
+//左侧栏的关闭事件
+$('.map_menu_close').click(function() {
+    $('.map_side_all_default').show();
+    $('.map_side_all_menu').hide();
+    // $('.map_side_all_menu').hide('easeOutQuad', function() {
+
+    // });
+
+});
+
+//logo右边的三个导航栏点击事件
+$('.map_three_nav').on('click', 'li', function(event) {
+
+    var target = event.target;
+    console.log(target);
+
+    switch (target.getAttribute('class')) {
+        case 'map_mendian':
+            // alert($(target).text());
+            location.href = "map.html";
+            break;
+        case 'map_my_account':
+
+            alert($(target).text());
+            break;
+        case 'map_menu':
+            alert($(target).text());
+            break;
+    }
+});
+
+//登录事件
+$('.map_login').click(function() {
+    alert('login');
+});
+
+//注册事件
+$('.map_regist_text').click(function() {
+    alert('regist');
+});
+
+//左侧竖排导航栏事件
+$('.map_menu_parent').on('click', "li", function() {
+    var target = event.target;
+    if (target.getAttribute('class') != 'map_menu_line') {
+        switch (target.getAttribute('class')) {
+            case 'map_menu_mendian':
+                alert($(target).text());
+                location.href = "map.html";
+                break;
+            case 'map_menu_xxjlb':
+
+                alert($(target).text());
+                break;
+            case 'map_menu':
+                alert($(target).text());
+                break;
+            default:
+                alert($(target).text());
+                break;
+        }
+    }
+
 });
